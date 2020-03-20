71a2deb5dd16744ff45c1dabb6b8c744590b0536
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-03-20 19:35:52 +0100

    Make Triangle class

---
+++ src/chapter11/Triangle/TestTriangle.java
@@ -0,0 +1,27 @@
+package chapter11.Triangle;
+
+import java.util.Scanner;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter sides(3): ");
+        double[] sides = new double[3];
+        for (int i = 0; i < sides.length; i++) {
+            sides[i] = input.nextDouble();
+        }
+        System.out.print("Enter color of the triangle: ");
+        String color = input.next();
+        System.out.print("Triangle should be filled? (true/false): ");
+        boolean filled = input.nextBoolean();
+        input.close();
+        Triangle triangle = new Triangle(sides[0], sides[1], sides[2]);
+        triangle.setColor(color);
+        triangle.setFilled(filled);
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+    }
+
+}
---
+++ src/chapter11/Triangle/Triangle.java
@@ -0,0 +1,46 @@
+package chapter11.Triangle;
+
+import chapter11.SimpleGeometricObject.SimpleGeometricObject;
+
+public class Triangle extends SimpleGeometricObject {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+
+    Triangle() {
+
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public double getArea() {
+        double area = 0;
+        double s = (side1 + side2 + side3) / 2.0;
+        area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        return area;
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    public String toString() {
+        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+    }
+}
