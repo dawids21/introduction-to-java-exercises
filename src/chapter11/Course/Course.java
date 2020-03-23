b018a1ab52f8814601038ccb41a978b0b4f1bf96
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-03-23 22:14:36 +0100

    Course class

---
+++ src/chapter11/Course/Course.java
@@ -0,0 +1,35 @@
+package chapter11.Course;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String courseName;
+    private ArrayList<String> students = new ArrayList<>();
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void addStudent(String student) {
+        if (!students.contains(student)) {
+            students.add(student);
+        }
+    }
+
+    public ArrayList<String> getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return students.size();
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        students.remove(student);
+    }
+
+}
