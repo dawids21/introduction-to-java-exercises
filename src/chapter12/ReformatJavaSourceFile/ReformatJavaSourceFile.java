858b8850262f157d1e1c11df227863f9be78b35b
dawids21 <dawid.stasiak21@gmail.com>
2020-04-05 12:30:08 +0200

    ReformatJavaSourceCode exercise

---
+++ src/chapter12/ReformatJavaSourceFile/ReformatJavaSourceFile.java
@@ -0,0 +1,51 @@
+package chapter12.ReformatJavaSourceFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReformatJavaSourceFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length != 1) {
+            System.out.println("Usage: java ReformatJavaSourceFile.java filename");
+            System.exit(1);
+        }
+        File inputFile = new File(args[0]);
+        if (!inputFile.exists()) {
+            System.out.println("File does not exist");
+            System.exit(2);
+        }
+        File outputFile = new File("new_" + args[0]);
+        if (outputFile.exists()) {
+            System.out.println("File alreay existx");
+            System.exit(3);
+        }
+
+        reformat(inputFile, outputFile);
+    }
+
+    private static void reformat(File inputFile, File outputFile) throws FileNotFoundException {
+        Pattern pattern = Pattern.compile("(?<white>\\s*)(?<bracket>\\{?)(?<line>.*)");
+        Matcher matcher;
+        String line;
+
+        try (Scanner input = new Scanner(inputFile); PrintWriter output = new PrintWriter(outputFile)) {
+            while (input.hasNextLine()) {
+                line = input.nextLine();
+                matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    if (matcher.group("bracket").length() == 1) {
+                        output.print(" {");
+                    } else {
+                        output.println();
+                    }
+                    output.print(matcher.group("white"));
+                    output.print(matcher.group("line"));
+                }
+            }
+        }
+    }
+}
