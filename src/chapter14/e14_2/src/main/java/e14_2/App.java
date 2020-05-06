eb13f72239c075db494f97f4ed73dced5efd1853
dawids21 <dawid.stasiak21@gmail.com>
2020-05-06 16:40:14 +0200

    Stub for application

---
+++ src/chapter14/e14_2/src/main/java/e14_2/App.java
@@ -0,0 +1,16 @@
+package e14_2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
