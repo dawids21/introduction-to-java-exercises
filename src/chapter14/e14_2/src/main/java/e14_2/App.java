75b8c9397e30be4063e5ba205f01f1ad6a4c89e8
dawids21 <dawid.stasiak21@gmail.com>
2020-05-07 09:37:20 +0200

    Finish exercise 14.2

--- src/chapter14/e14_2/src/main/java/e14_2/App.java
+++ src/chapter14/e14_2/src/main/java/e14_2/App.java
@@ -1,12 +1,30 @@
 package e14_2;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Image circle = new Image("O.png", 0, 90, true, true);
+        Image cross = new Image("X.png", 0, 90, true, true);
+        GridPane pane = new GridPane();
+        pane.add(new ImageView(circle), 0, 0);
+        pane.add(new ImageView(circle), 2, 0);
+        pane.add(new ImageView(cross), 1, 0);
+        pane.add(new ImageView(cross), 0, 1);
+        pane.add(new ImageView(cross), 2, 1);
+        pane.add(new ImageView(cross), 0, 2);
+        pane.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(pane, 500, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
