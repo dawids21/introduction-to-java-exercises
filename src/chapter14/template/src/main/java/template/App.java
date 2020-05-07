f1c6b266799e33aeacc1a429456f01ac44229376
dawids21 <dawid.stasiak21@gmail.com>
2020-05-07 18:23:49 +0200

    Template for exercises

---
+++ src/chapter14/template/src/main/java/template/App.java
@@ -0,0 +1,21 @@
+package template;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
