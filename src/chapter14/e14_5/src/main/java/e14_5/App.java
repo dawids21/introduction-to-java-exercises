4c720a65d2a026ed089adefc292c65125bde4765
dawids21 <dawid.stasiak21@gmail.com>
2020-05-07 18:33:46 +0200

    Create e14_5 project

--- src/chapter14/e14_5/src/main/java/e14_5/App.java
+++ src/chapter14/e14_5/src/main/java/e14_5/App.java
@@ -1,13 +1,21 @@
 package e14_5;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
