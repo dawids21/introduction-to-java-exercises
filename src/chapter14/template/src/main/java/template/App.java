c0394978b22d4eb0080215069ff7299faec9ccba
dawids21 <dawid.stasiak21@gmail.com>
2020-05-09 10:26:42 +0200

    Add WIDTH and HEIGHT to template

--- src/chapter14/template/src/main/java/template/App.java
+++ src/chapter14/template/src/main/java/template/App.java
@@ -1,20 +1,24 @@
 package template;
 
+import org.graalvm.compiler.phases.common.NodeCounterPhase.Stage;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         Pane pane = new Pane();
-        Scene scene = new Scene(pane, 500, 500);
+        Scene scene = new Scene(pane, WIDTH, HEIGHT);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
