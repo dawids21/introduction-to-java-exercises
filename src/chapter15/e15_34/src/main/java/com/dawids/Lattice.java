e4aedda98da5cb822d3cbd7590ddb5134f5e650c
dawids21 <dawid.stasiak21@gmail.com>
2020-05-25 18:12:52 +0200

    Add drawLines function

--- src/chapter15/e15_34/src/main/java/com/dawids/Lattice.java
+++ src/chapter15/e15_34/src/main/java/com/dawids/Lattice.java
@@ -1,4 +1,19 @@
 package com.dawids;
 
-public class Lattice {
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+
+public class Lattice extends Pane {
+    public Lattice() {
+        this(10);
+    }
+
+    public Lattice(int size) {
+        getChildren().add(drawLines(size));
+    }
+
+    private Group drawLines(int size) {
+        var grid = new Group();
+        return grid;
+    }
 }
