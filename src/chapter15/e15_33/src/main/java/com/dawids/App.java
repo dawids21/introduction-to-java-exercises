62ef3f2740259fa36e862c817eb5edec2e6ec51f
dawids21 <dawid.stasiak21@gmail.com>
2020-05-25 07:52:04 +0200

    Make constants WIDTH and HEIGHT

--- src/chapter15/e15_33/src/main/java/com/dawids/App.java
+++ src/chapter15/e15_33/src/main/java/com/dawids/App.java
@@ -10,10 +10,14 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
     @Override
     public void start(Stage stage) {
         var beanMachinePane = new BeanMachinePane(640);
         var scene = new Scene(beanMachinePane, 640, 480);
+        var scene = new Scene(mainPane, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.show();
     }
