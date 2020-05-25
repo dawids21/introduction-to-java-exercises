2964ac1c811579ca0c681bc43656f756a0d1ff02
dawids21 <dawid.stasiak21@gmail.com>
2020-05-25 22:42:18 +0200

    Add head of walk

--- src/chapter15/e15_34/src/main/java/com/dawids/LatticePane.java
+++ src/chapter15/e15_34/src/main/java/com/dawids/LatticePane.java
@@ -6,12 +6,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class LatticePane extends Pane {
+
+    private Point head = new Point();
+
     public LatticePane() {
         this(10.0);
     }
 
     public LatticePane(double size) {
         getChildren().add(drawLines(size));
+        head.setX((int) (size / 2));
+        head.setY((int) (size / 2));
     }
 
     private Group drawLines(double size) {
