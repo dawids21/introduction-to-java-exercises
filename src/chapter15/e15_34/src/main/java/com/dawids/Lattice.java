f6e1865c90375f43430c49b61662608033daf85b
dawids21 <dawid.stasiak21@gmail.com>
2020-05-25 18:23:47 +0200

    Draw horizontal lines

--- src/chapter15/e15_34/src/main/java/com/dawids/Lattice.java
+++ src/chapter15/e15_34/src/main/java/com/dawids/Lattice.java
@@ -2,18 +2,28 @@ package com.dawids;
 
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 public class Lattice extends Pane {
     public Lattice() {
-        this(10);
+        this(10.0);
     }
 
-    public Lattice(int size) {
+    public Lattice(double size) {
         getChildren().add(drawLines(size));
     }
 
-    private Group drawLines(int size) {
+    private Group drawLines(double size) {
         var grid = new Group();
+        for (var i = 0; i < size; i++) {
+            var line = new Line();
+            line.startYProperty().bind(heightProperty().divide(size).multiply(i));
+            line.endXProperty().bind(widthProperty());
+            line.endYProperty().bind(line.startYProperty());
+            line.setStroke(Color.GRAY);
+            grid.getChildren().add(line);
+        }
         return grid;
     }
 }
