e7864e5c7fc29d1116b990b4374ba6824ec0b887
dawids21 <dawid.stasiak21@gmail.com>
2020-05-26 07:59:31 +0200

    Add button to stage

--- src/chapter15/e15_34/src/main/java/com/dawids/App.java
+++ src/chapter15/e15_34/src/main/java/com/dawids/App.java
@@ -1,7 +1,9 @@
 package com.dawids;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,7 +16,12 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         var borderPane = new BorderPane();
+        var startButton = new Button("Start");
+
+        BorderPane.setAlignment(startButton, Pos.CENTER);
+
         borderPane.setCenter(new LatticePane(20));
+        borderPane.setBottom(startButton);
         var scene = new Scene(borderPane, 640, 480);
         stage.setScene(scene);
         stage.show();
