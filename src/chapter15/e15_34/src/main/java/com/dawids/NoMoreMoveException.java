68b32932443bb92b350659c82c34ce187657eb9b
dawids21 <dawid.stasiak21@gmail.com>
2020-05-26 08:06:11 +0200

    Add methods to NoMoreMoveException

--- src/chapter15/e15_34/src/main/java/com/dawids/NoMoreMoveException.java
+++ src/chapter15/e15_34/src/main/java/com/dawids/NoMoreMoveException.java
@@ -1,4 +1,28 @@
 package com.dawids;
 
 public class NoMoreMoveException extends Exception {
+    private final Point head;
+    private final boolean reachBoundary;
+
+    public NoMoreMoveException() {
+        head = new Point(0, 0);
+        reachBoundary = false;
+    }
+
+    public NoMoreMoveException(Point head, boolean reachBoundary) {
+        this.head = head;
+        this.reachBoundary = reachBoundary;
+    }
+
+    public boolean isReachBoundary() {
+        return reachBoundary;
+    }
+
+    public int getHeadX() {
+        return head.getX();
+    }
+
+    public int getHeadY() {
+        return head.getY();
+    }
 }
