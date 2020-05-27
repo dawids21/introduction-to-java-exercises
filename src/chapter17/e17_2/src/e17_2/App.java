101e6f5181e2648f33388815ee44940bfb95e4bb
dawids21 <dawid.stasiak21@gmail.com>
2020-05-27 15:34:21 +0200

    Complete exercise 17.2

--- src/chapter17/e17_2/src/e17_2/App.java
+++ src/chapter17/e17_2/src/e17_2/App.java
@@ -1,4 +1,31 @@
 package e17_2;
 
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Random;
+
 public class App {
+    public static void main(String[] args) {
+        try (var outputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("Exercise17_02.dat",
+                                                                                                   true)));) {
+            var rand = new Random();
+            for (var i = 0; i < 100; i++) {
+                outputStream.writeInt(rand.nextInt());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        var sum = new BigInteger(String.valueOf(0));
+        try (var inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream("Exercise17_02.dat")))) {
+            while (true) {
+                sum = sum.add(BigInteger.valueOf(inputStream.readInt()));
+            }
+        } catch (EOFException e) {
+            System.out.println("Sum is " + sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
