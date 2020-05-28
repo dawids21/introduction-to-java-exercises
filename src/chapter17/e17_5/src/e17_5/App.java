53509e518eab930675c010319d3fefe72c94c3bc
dawids21 <dawid.stasiak21@gmail.com>
2020-05-28 07:43:02 +0200

    Complete exercise 17.5

--- src/chapter17/e17_5/src/e17_5/App.java
+++ src/chapter17/e17_5/src/e17_5/App.java
@@ -1,4 +1,28 @@
 package e17_5;
 
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
+
 public class App {
+    public static void main(String[] args) {
+        try (var outputStream = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Exercise17_05.dat")))) {
+            outputStream.writeObject(new int[]{1, 2, 3, 4, 5});
+            outputStream.writeObject(new Date());
+            outputStream.writeDouble(5.5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (var inputStream = new ObjectInputStream(new BufferedInputStream(new FileInputStream("Exercise17_05.dat")))) {
+            System.out.println(Arrays.toString(((int[]) inputStream.readObject())));
+            System.out.println(inputStream.readObject());
+            System.out.println(inputStream.readDouble());
+        } catch (EOFException e) {
+            System.out.println("Reading complete");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
