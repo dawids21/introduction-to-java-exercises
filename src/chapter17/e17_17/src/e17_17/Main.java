8c2e4478ae0ba09a2a7cc9674694224ad09c260b
dawids21 <dawid.stasiak21@gmail.com>
2020-05-29 16:52:39 +0200

    Write test program

--- src/chapter17/e17_17/src/e17_17/Main.java
+++ src/chapter17/e17_17/src/e17_17/Main.java
@@ -1,8 +1,15 @@
 package e17_17;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try (var outputBit = new BitOutputStream(new File("Exercise17_17.dat"))){
+            outputBit.writeBit("010000100100001001101");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
