948cd1c0caed3df0d426eb5b477e1c9cf91af712
dawids21 <dawid.stasiak21@gmail.com>
2020-05-29 20:51:25 +0200

    Add BitOutputStream class

---
+++ src/chapter17/e17_20/src/main/java/com/dawids/BitOutputStream.java
@@ -0,0 +1,49 @@
+package com.dawids;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BitOutputStream implements AutoCloseable {
+
+    private FileOutputStream output;
+    private byte writeByte = 0;
+    private int byteSize = 0;
+
+    public BitOutputStream(File output) throws IOException {
+        this.output = new FileOutputStream(output);
+    }
+
+    public void writeBit(char bit) throws IOException {
+        if (bit != '1' && bit != '0') {
+            throw new IllegalArgumentException("Bit must be 0 or 1");
+        } else {
+            writeByte <<= 1;
+            writeByte |= (bit == '1' ? 1 : 0);
+            byteSize++;
+        }
+        if (byteSize == 8) {
+            output.write(writeByte);
+            byteSize = 0;
+            writeByte = 0;
+        }
+    }
+
+    public void writeBit(String bit) throws IOException {
+        for (var i = 0; i < bit.length(); i++) {
+            writeBit(bit.charAt(i));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (output != null) {
+            writeByte <<= 8 - byteSize;
+            output.write(writeByte);
+            writeByte = 0;
+            byteSize = 0;
+            output.close();
+            output = null;
+        }
+    }
+}
