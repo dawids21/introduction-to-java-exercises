d390752bcc5ee5b8d151537471f402367024f738
dawids21 <dawid.stasiak21@gmail.com>
2020-05-31 11:44:16 +0200

    Trim strings in Person if they are too long

--- src/chapter18/e17_9/src/main/java/com/dawids/Person.java
+++ src/chapter18/e17_9/src/main/java/com/dawids/Person.java
@@ -16,8 +16,20 @@ public class Person {
     }
 
     public Person(String name, String street, String city, String state, String zip) {
-        if (name.length() > 32 || street.length() > 32 || city.length() > 20 || state.length() > 2 || zip.length() > 5) {
-            throw new IllegalArgumentException();
+        if (name.length() > 32) {
+            name = name.substring(0, 32);
+        }
+        if (street.length() > 32) {
+            street = street.substring(0, 32);
+        }
+        if (city.length() > 20) {
+            city = city.substring(0, 20);
+        }
+        if (state.length() > 2) {
+            state = state.substring(0, 2);
+        }
+        if (zip.length() > 5) {
+            zip = zip.substring(0, 5);
         }
         this.name = String.format("%-32s", name);
         this.street = String.format("%-32s", street);
