b13878ee190dd578ddcb3ae004c19bf597023703
dawids21 <dawid.stasiak21@gmail.com>
2020-06-09 12:09:53 +0200

    Add next button action

--- src/chapter18/e17_9/src/main/java/com/dawids/App.java
+++ src/chapter18/e17_9/src/main/java/com/dawids/App.java
@@ -69,6 +69,15 @@ public class App extends Application {
             setTextFields(person);
         });
         //todo button next
+        buttons.get(Buttons.NEXT).setOnAction(event -> {
+            if (index < 0) {
+                index = 0;
+            } else {
+                index++;
+            }
+            Person person = readEntry(index);
+            setTextFields(person);
+        });
         //todo button previous
         buttons.get(Buttons.LAST).setOnAction(event -> {
             try (var inputFile = new RandomAccessFile(FILE_NAME, "r")) {
