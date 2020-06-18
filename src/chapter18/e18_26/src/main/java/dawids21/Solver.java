bdcdf21108d26c7d6bd11e3804c8100eb55f3d2b
dawids21 <dawid.stasiak21@gmail.com>
2020-06-18 15:46:43 +0200

    Add constructors to Solver

--- src/chapter18/e18_26/src/main/java/dawids21/Solver.java
+++ src/chapter18/e18_26/src/main/java/dawids21/Solver.java
@@ -1,5 +1,15 @@
 package dawids21;
 
 class Solver {
-    
+
+    private final Point head = new Point();
+    private final Maze mazeLayout;
+
+    Solver() {
+        mazeLayout = new Maze();
+    }
+
+    Solver(Maze mazeLayout) {
+        this.mazeLayout = mazeLayout;
+    }
 }
