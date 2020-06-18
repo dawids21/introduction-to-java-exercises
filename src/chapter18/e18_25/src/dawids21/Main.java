f68b20c072e7a0d480901731d91c0c4e5367dea7
dawids21 <dawid.stasiak21@gmail.com>
2020-06-18 07:33:54 +0200

    Finish exercise 18.25

--- src/chapter18/e18_25/src/dawids21/Main.java
+++ src/chapter18/e18_25/src/dawids21/Main.java
@@ -3,6 +3,21 @@ package dawids21;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        displayPermutation("abc");
+    }
+
+    public static void displayPermutation(String str) {
+        displayPermutation("", str);
+    }
+
+    private static void displayPermutation(String permutation, String str) {
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            displayPermutation(permutation + ch, str.replaceFirst(String.valueOf(ch), ""));
+        }
     }
 }
