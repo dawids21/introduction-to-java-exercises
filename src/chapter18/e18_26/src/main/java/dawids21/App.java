94f9b7484a2fa98df90984302e63491ff404b92e
dawids21 <dawid.stasiak21@gmail.com>
2020-06-18 15:18:33 +0200

    Create project for exercise 18.26

---
+++ src/chapter18/e18_26/src/main/java/dawids21/App.java
@@ -0,0 +1,20 @@
+package dawids21;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/chapter18/e18_26/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module dawids21 {
+    requires javafx.controls;
+    exports dawids21;
+}
