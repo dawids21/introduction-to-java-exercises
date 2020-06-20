aaba0a0d7a6e0be6fea6d66561fbbc139a4ac321
dawids21 <dawid.stasiak21@gmail.com>
2020-06-20 13:24:10 +0200

    Change name in Maze from layout to mazeLayout

--- src/chapter18/e18_26/src/main/java/dawids21/Maze.java
+++ src/chapter18/e18_26/src/main/java/dawids21/Maze.java
@@ -5,12 +5,12 @@ public class Maze {
     public static final int NUM_OF_ROWS = 8;
     public static final int NUM_OF_COLUMNS = 8;
 
-    private final TypesOfCells[][] layout = new TypesOfCells[NUM_OF_ROWS][NUM_OF_COLUMNS];
+    private final TypesOfCells[][] mazeLayout = new TypesOfCells[NUM_OF_ROWS][NUM_OF_COLUMNS];
 
     public Maze() {
         for (int i = 0; i < NUM_OF_ROWS; i++) {
             for (int j = 0; j < NUM_OF_COLUMNS; j++) {
-                layout[i][j] = TypesOfCells.FREE;
+                mazeLayout[i][j] = TypesOfCells.FREE;
             }
         }
     }
@@ -22,7 +22,7 @@ public class Maze {
         if (point.getY() < 0 || point.getY() >= NUM_OF_ROWS) {
             throw new IllegalArgumentException("Y coordinate is out of bound");
         }
-        layout[point.getX()][point.getY()] = type;
+        mazeLayout[point.getX()][point.getY()] = type;
     }
 
     public TypesOfCells getCell(Point point) {
@@ -32,14 +32,14 @@ public class Maze {
         if (point.getY() < 0 || point.getY() >= NUM_OF_ROWS) {
             throw new IllegalArgumentException("Y coordinate is out of bound");
         }
-        return layout[point.getX()][point.getY()];
+        return mazeLayout[point.getX()][point.getY()];
     }
 
     public void clearPath() {
         for (int i = 0; i < NUM_OF_ROWS; i++) {
             for (int j = 0; j < NUM_OF_COLUMNS; j++) {
-                if (layout[i][j] == TypesOfCells.OCCUPIED) {
-                    layout[i][j] = TypesOfCells.FREE;
+                if (mazeLayout[i][j] == TypesOfCells.OCCUPIED) {
+                    mazeLayout[i][j] = TypesOfCells.FREE;
                 }
             }
         }
