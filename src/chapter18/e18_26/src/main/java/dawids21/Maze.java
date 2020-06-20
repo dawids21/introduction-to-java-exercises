2ed877bd717bb5520e31574a22a10519396d413b
dawids21 <dawid.stasiak21@gmail.com>
2020-06-20 13:14:31 +0200

    Introduce constants in Maze class for number of rows and columns

--- src/chapter18/e18_26/src/main/java/dawids21/Maze.java
+++ src/chapter18/e18_26/src/main/java/dawids21/Maze.java
@@ -1,11 +1,15 @@
 package dawids21;
 
 public class Maze {
-    private final TypesOfCells[][] layout = new TypesOfCells[8][8];
+
+    public static final int NUM_OF_ROWS = 8;
+    public static final int NUM_OF_COLUMNS = 8;
+
+    private final TypesOfCells[][] layout = new TypesOfCells[NUM_OF_ROWS][NUM_OF_COLUMNS];
 
     public Maze() {
-        for (int i = 0; i < layout.length; i++) {
-            for (int j = 0; j < layout[0].length; j++) {
+        for (int i = 0; i < NUM_OF_ROWS; i++) {
+            for (int j = 0; j < NUM_OF_COLUMNS; j++) {
                 layout[i][j] = TypesOfCells.FREE;
             }
         }
@@ -20,8 +24,8 @@ public class Maze {
     }
 
     public void clearPath() {
-        for (int i = 0; i < layout.length; i++) {
-            for (int j = 0; j < layout[0].length; j++) {
+        for (int i = 0; i < NUM_OF_ROWS; i++) {
+            for (int j = 0; j < NUM_OF_COLUMNS; j++) {
                 if (layout[i][j] == TypesOfCells.OCCUPIED) {
                     layout[i][j] = TypesOfCells.FREE;
                 }
