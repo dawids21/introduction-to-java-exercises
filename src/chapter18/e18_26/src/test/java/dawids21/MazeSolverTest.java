c08d818065760bdeac5879443712b2d949968e46
dawids21 <dawid.stasiak21@gmail.com>
2020-06-20 14:11:03 +0200

    Add DisplayName to testInitMaze

--- src/chapter18/e18_26/src/test/java/dawids21/MazeSolverTest.java
+++ src/chapter18/e18_26/src/test/java/dawids21/MazeSolverTest.java
@@ -1,6 +1,7 @@
 package dawids21;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -28,7 +29,7 @@ class MazeSolverTest {
             TypesOfCells[][] actual = maze.getMazeLayout();
             for (var i = 0; i < Maze.NUM_OF_COLUMNS; i++) {
                 for (var j = 0; j < Maze.NUM_OF_ROWS; j++) {
-                    assertEquals(expected[i][j], actual[i][j], "Should be free");
+                    assertEquals(expected[i][j], actual[i][j]);
                 }
             }
         }
