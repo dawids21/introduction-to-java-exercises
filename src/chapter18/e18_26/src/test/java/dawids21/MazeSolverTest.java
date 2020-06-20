a218f400285e4f38f5e14113632f9ab80fe16f91
dawids21 <dawid.stasiak21@gmail.com>
2020-06-20 13:39:15 +0200

    Create test for Maze init

--- src/chapter18/e18_26/src/test/java/dawids21/MazeSolverTest.java
+++ src/chapter18/e18_26/src/test/java/dawids21/MazeSolverTest.java
@@ -1,4 +1,36 @@
 package dawids21;
 
-class MazeTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MazeSolverTest {
+
+    @Nested
+    class MazeTest {
+        Maze maze;
+
+        @BeforeEach
+        void setUp() {
+            maze = new Maze();
+        }
+
+        @Test
+        void testInitMaze() {
+            TypesOfCells[][] expected = new TypesOfCells[8][8];
+            for (var i = 0; i < Maze.NUM_OF_COLUMNS; i++) {
+                for (var j = 0; j < Maze.NUM_OF_ROWS; j++) {
+                    expected[i][j] = TypesOfCells.FREE;
+                }
+            }
+            TypesOfCells[][] actual = maze.getMazeLayout();
+            for (var i = 0; i < Maze.NUM_OF_COLUMNS; i++) {
+                for (var j = 0; j < Maze.NUM_OF_ROWS; j++) {
+                    assertEquals(expected[i][j], actual[i][j], "Should be free");
+                }
+            }
+        }
+    }
 }
