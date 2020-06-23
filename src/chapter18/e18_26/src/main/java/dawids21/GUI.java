f63742ebe0168dd869c562c4fe20158a888a0773
dawids21 <dawid.stasiak21@gmail.com>
2020-06-23 08:16:25 +0200

    Create GUI class

---
+++ src/chapter18/e18_26/src/main/java/dawids21/GUI.java
@@ -0,0 +1,15 @@
+package dawids21;
+
+import javafx.stage.Stage;
+
+public class GUI {
+    private final Stage primaryStage;
+
+    public GUI() {
+        primaryStage = null;
+    }
+
+    public GUI(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+}
