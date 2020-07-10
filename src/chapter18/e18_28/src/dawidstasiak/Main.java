ffcfccc6881a662d4c6f924682122614e3b36e7c
dawids21 <dawid.stasiak21@gmail.com>
2020-07-10 12:14:45 +0200

    Finish exercise 18.28

--- src/chapter18/e18_28/src/dawidstasiak/Main.java
+++ src/chapter18/e18_28/src/dawidstasiak/Main.java
@@ -1,8 +1,39 @@
 package dawidstasiak;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if (args.length != 1) {
+            System.out.println("Wrong arguments");
+            System.exit(1);
+        }
+
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println("Directory does not exist");
+            System.exit(2);
+        }
+
+        var stackOfFiles = new ArrayDeque<File>();
+        var size = 0L;
+        stackOfFiles.push(file);
+        while (!stackOfFiles.isEmpty()) {
+            var currentFile = stackOfFiles.pop();
+            if (currentFile.isDirectory()) {
+                for (File fileInDirectory : Objects.requireNonNull(currentFile.listFiles())) {
+                    stackOfFiles.push(fileInDirectory);
+                }
+            } else {
+                size += currentFile.length();
+            }
+        }
+        System.out.println("Directory size: " + size);
     }
 }
