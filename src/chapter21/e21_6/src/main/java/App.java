86be260ef51b961d79e312f2923f56bc349c46c4
dawids21 <dawid.stasiak21@gmail.com>
2020-07-24 10:52:55 +0200

    Finish exercise 21.6

---
+++ src/chapter21/e21_6/src/main/java/App.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var mapOfOccurrences = new HashMap<Integer, Integer>();
+
+        System.out.print("Enter the numbers: ");
+        String[] numbers = input.nextLine().split("\\s+");
+        input.close();
+
+        Arrays.stream(numbers).filter(App::isInteger).map(Integer::parseInt).forEach(integer -> {
+            if (mapOfOccurrences.containsKey(integer)) {
+                mapOfOccurrences.put(integer, mapOfOccurrences.get(integer) + 1);
+            } else {
+                mapOfOccurrences.put(integer, 1);
+            }
+        });
+
+        int maxOccurrences = Collections.max(mapOfOccurrences.values());
+        mapOfOccurrences.entrySet()
+                        .stream()
+                        .filter(entry -> entry.getValue() == maxOccurrences)
+                        .forEach(entry -> System.out.print(entry.getKey() + " "));
+    }
+
+    private static boolean isInteger(String number) {
+        boolean integer;
+        try {
+            Integer.parseInt(number);
+            integer = true;
+        } catch (NumberFormatException e) {
+            integer = false;
+        }
+        return integer;
+    }
+}
