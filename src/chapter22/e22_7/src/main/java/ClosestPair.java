e5c410446d4c8b09277312921a39450ca5bf8f75
dawids21 <dawid.stasiak21@gmail.com>
2020-08-13 10:13:00 +0200

    Write test for finding the closest points

--- src/chapter22/e22_7/src/main/java/ClosestPair.java
+++ src/chapter22/e22_7/src/main/java/ClosestPair.java
@@ -1,3 +1,8 @@
+import java.util.List;
+
 public class ClosestPair {
 
+    public static Pair find(List<? extends Point2D> points) {
+        return null;
+    }
 }
--- src/chapter22/e22_7/src/test/java/ClosestPairTest.java
+++ src/chapter22/e22_7/src/test/java/ClosestPairTest.java
@@ -1,3 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ClosestPairTest {
 
+    @Test
+    void return_pair_of_points_that_are_closest_to_each_other() {
+        var points = new ArrayList<Point2D>();
+        points.add(new Point2D(0, 9));
+        points.add(new Point2D(5, 8));
+        points.add(new Point2D(2, 7));
+        points.add(new Point2D(7, 6));
+        points.add(new Point2D(4, 5));
+        points.add(new Point2D(1, 4));
+        points.add(new Point2D(3, 4));
+        points.add(new Point2D(2, 2));
+        points.add(new Point2D(6, 2));
+        points.add(new Point2D(4, 0));
+
+        Pair closestPair = ClosestPair.find(points);
+        Pair expectedPair = new Pair(new Point2D(3, 4), new Point2D(4, 5));
+
+        assertEquals(expectedPair, closestPair);
+    }
 }
