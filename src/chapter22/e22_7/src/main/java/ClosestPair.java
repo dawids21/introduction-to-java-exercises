7fba518654dbd020e480910b0fb7f28d59e5911d
dawids21 <dawid.stasiak21@gmail.com>
2020-08-13 10:51:49 +0200

    Write stub for find method

--- src/chapter22/e22_7/src/main/java/ClosestPair.java
+++ src/chapter22/e22_7/src/main/java/ClosestPair.java
@@ -1,8 +1,30 @@
-import java.util.Set;
+import java.util.Collection;
+import java.util.TreeSet;
 
 public class ClosestPair {
 
-    public static Pair find(Set<? extends Point2D> points) {
+    public static Pair find(Collection<? extends Point2D> points) {
+
+        var orderedPoints = new TreeSet<Point2D>((p1, p2) -> {
+            int compare;
+            if (p1.getX() != p2.getX()) {
+                compare = p1.getX() > p2.getX() ? 1 : -1;
+            } else {
+                if (p1.getY() == p2.getY()) {
+                    compare = 0;
+                } else {
+                    compare = p1.getY() > p2.getY() ? 1 : -1;
+                }
+            }
+            return compare;
+        });
+
+        orderedPoints.addAll(points);
+
+        return recursivelyFindDistance(orderedPoints);
+    }
+
+    private static Pair recursivelyFindDistance(TreeSet<Point2D> points) {
         return null;
     }
 }
