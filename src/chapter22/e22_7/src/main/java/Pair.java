68c6cb627b8476567ff176065ece04a4b777dc84
dawids21 <dawid.stasiak21@gmail.com>
2020-08-13 10:12:00 +0200

    Write Pair class

--- src/chapter22/e22_7/src/main/java/Pair.java
+++ src/chapter22/e22_7/src/main/java/Pair.java
@@ -1,3 +1,23 @@
 public class Pair {
 
+    private final Point2D p1;
+    private final Point2D p2;
+
+    public Pair(Point2D p1, Point2D p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point2D getP1() {
+        return p1;
+    }
+
+    public Point2D getP2() {
+        return p2;
+    }
+
+    public double getDistance() {
+        return Math.sqrt(Math.pow(getP1().getX() - getP2().getX(), 2) +
+                         Math.pow(getP1().getY() - getP2().getY(), 2));
+    }
 }
