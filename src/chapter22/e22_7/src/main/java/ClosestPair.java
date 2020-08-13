eea0c8e17051775a1634c0650ba68881533debca
dawids21 <dawid.stasiak21@gmail.com>
2020-08-13 13:44:39 +0200

    Add comments to algorithm

--- src/chapter22/e22_7/src/main/java/ClosestPair.java
+++ src/chapter22/e22_7/src/main/java/ClosestPair.java
@@ -24,6 +24,7 @@ public class ClosestPair {
 
     private static Pair recursivelyFindDistance(List<Point2D> points) {
 
+        // Base cases
         if (points.size() == 3) {
             Pair[] pairs = {new Pair(points.get(0), points.get(1)),
                             new Pair(points.get(1), points.get(2)),
@@ -33,6 +34,7 @@ public class ClosestPair {
             return new Pair(points.get(0), points.get(1));
         }
 
+        // Sort points according to their Y coordinates
         var orderedByY = new ArrayList<>(points);
         orderedByY.sort((p1, p2) -> {
             int compare;
@@ -48,6 +50,7 @@ public class ClosestPair {
             return compare;
         });
 
+        // Divide points between two sides
         var leftSide = new ArrayList<Point2D>();
         var rightSide = new ArrayList<Point2D>();
         for (int i = 0; i < points.size(); i++) {
@@ -57,13 +60,20 @@ public class ClosestPair {
                 rightSide.add(points.get(i));
             }
         }
+
+        // Recursively calculate the closest pairs of point in each side
         var closestPairInLeft = recursivelyFindDistance(leftSide);
         var closestPairInRight = recursivelyFindDistance(rightSide);
+
+        // Take last point in left side as a midpoint
         var midPoint = leftSide.get(leftSide.size() - 1);
 
+        // Get closer of the two pairs in both sides
         var minPair = closestPairInLeft.compareTo(closestPairInRight) <= 0 ?
                  closestPairInLeft : closestPairInRight;
         var minDistance = minPair.getDistance();
+
+        // Get points that may be closer than minPair
         var stripL = new ArrayList<Point2D>();
         var stripR = new ArrayList<Point2D>();
         for (var point : orderedByY) {
@@ -76,6 +86,7 @@ public class ClosestPair {
             }
         }
 
+        // Check possible points
         for (var point : stripL) {
             int i = 0;
             while (i < stripR.size() && stripR.get(i)
