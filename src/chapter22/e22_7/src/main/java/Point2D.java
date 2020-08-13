7ea7dbb6335c7a55a5033640da4d481c88ff3df3
dawids21 <dawid.stasiak21@gmail.com>
2020-08-13 08:40:43 +0200

    Start implementing the closest pair problem

---
+++ src/chapter22/e22_7/src/main/java/Point2D.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Point2D {
+
+    private int x = 0;
+    private int y = 0;
+
+    public Point2D() {
+
+    }
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point2D point = (Point2D) o;
+        return getX() == point.getX() &&
+                getY() == point.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
