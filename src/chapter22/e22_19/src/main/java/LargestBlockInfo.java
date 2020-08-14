8a822e2fb951aa22b87ede1b4880ce5b2a4314d4
dawids21 <dawid.stasiak21@gmail.com>
2020-08-14 09:26:01 +0200

    Create class LargestBlockInfo

--- src/chapter22/e22_19/src/main/java/LargestBlockInfo.java
+++ src/chapter22/e22_19/src/main/java/LargestBlockInfo.java
@@ -1,7 +1,49 @@
+import java.util.Objects;
+
 public class LargestBlockInfo {
 
+    private final int x;
+    private final int y;
+    private final int size;
+
     public LargestBlockInfo(int x, int y, int size) {
-       //TODO implement constructor
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LargestBlockInfo that = (LargestBlockInfo) o;
+        return getX() == that.getX() && getY() == that.getY() &&
+               getSize() == that.getSize();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), getSize());
+    }
+
+    @Override
+    public String toString() {
+        return "LargestBlockInfo{" + "x=" + x + ", y=" + y + ", size=" + size + '}';
     }
 }
