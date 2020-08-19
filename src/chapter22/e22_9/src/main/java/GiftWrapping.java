375ce1d9c310ebe119fc03e4c7fd754c15a8882d
dawids21 <dawid.stasiak21@gmail.com>
2020-08-19 08:27:01 +0200

    Write test and stub object

---
+++ src/chapter22/e22_9/src/main/java/GiftWrapping.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class GiftWrapping {
+
+    public GiftWrapping(List<? extends Point2D> points) {
+        //TODO implement GiftWrapping
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public List<Point2D> getSolution() {
+        //TODO implement getSolution
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ src/chapter22/e22_9/src/test/java/GiftWrappingTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GiftWrappingTest {
+
+    @Test
+    void return_convex_hull_from_all_points() {
+        var points = new ArrayList<Point2D>();
+        points.add(new Point2D(1, 1));
+        points.add(new Point2D(4, 2));
+        points.add(new Point2D(6, 6));
+        points.add(new Point2D(7, 6));
+        points.add(new Point2D(4, 4));
+        points.add(new Point2D(4, 6.5));
+        points.add(new Point2D(3, 4));
+        points.add(new Point2D(1, 7));
+
+        var giftWrapping = new GiftWrapping(points);
+        var convexHull = giftWrapping.getSolution();
+
+        List<Point2D> expected = new ArrayList<>();
+        expected.add(new Point2D(1, 1));
+        expected.add(new Point2D(4, 2));
+        expected.add(new Point2D(7, 6));
+        expected.add(new Point2D(4, 6.5));
+        expected.add(new Point2D(1, 7));
+
+        assertEquals(expected, convexHull);
+    }
+}
