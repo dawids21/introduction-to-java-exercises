b8b67dd496bcb5e3684e64daa9ecaca2fbd81ba3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-08-27 08:59:36 +0200

    Create test for graham algorithm

---
+++ src/chapter22/e22_11/src/main/java/GrahamAlgortihm.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrahamAlgortihm {
+
+    public GrahamAlgortihm(ArrayList<Point2D> points) {
+
+    }
+
+    public List<Point2D> getSolution() {
+        //TODO implement getSolution
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ src/chapter22/e22_11/src/main/java/Point2D.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+
+public class Point2D {
+
+    private double x;
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point2D point = (Point2D) o;
+        return getX() == point.getX() &&
+                getY() == point.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
