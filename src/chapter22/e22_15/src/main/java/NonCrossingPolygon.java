05bd7680a9ee6044bf4e6f11d9e66478038caf28
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-08-28 07:49:03 +0200

    Create test for exercise

--- src/chapter22/e22_15/src/main/java/NonCrossingPolygon.java
+++ src/chapter22/e22_15/src/main/java/NonCrossingPolygon.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NonCrossingPolygon {
 
+    public NonCrossingPolygon(List<Point2D> points) {
+        //TODO implement NonCrossingPolygon
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public List<Point2D> solve() {
+        //TODO implement solve
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
--- src/chapter22/e22_15/src/test/java/NonCrossingPolygonTest.java
+++ src/chapter22/e22_15/src/test/java/NonCrossingPolygonTest.java
@@ -1,3 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 class NonCrossingPolygonTest {
 
+    @Test
+    @DisplayName("Return ordered list of points that forms non-cross polygon")
+    void return_ordered_list_of_points_that_forms_non_cross_polygon() {
+        var points = new ArrayList<Point2D>();
+        points.add(new Point2D(1, 1));
+        points.add(new Point2D(1, 4));
+        points.add(new Point2D(2, 2));
+        points.add(new Point2D(2, 6));
+        points.add(new Point2D(5, 1));
+        points.add(new Point2D(5, 4));
+
+        var nonCrossingPolygon = new NonCrossingPolygon(points);
+
+        var expected = new ArrayList<Point2D>();
+        points.add(new Point2D(5, 1));
+        points.add(new Point2D(5, 4));
+        points.add(new Point2D(2, 6));
+        points.add(new Point2D(1, 4));
+        points.add(new Point2D(2, 2));
+        points.add(new Point2D(1, 1));
+
+        Assertions.assertEquals(expected, nonCrossingPolygon.solve());
+    }
 }
