d9ce6343bd6bd64ff28fce8f3dc48ba06961faf9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-09-17 13:13:48 +0200

    Write e 23.1

--- src/chapter23/e23_1/src/main/java/Sort.java
+++ src/chapter23/e23_1/src/main/java/Sort.java
@@ -4,9 +4,37 @@ public class Sort {
 
     public static <E extends Comparable<E>> void bubbleSort(E[] list) {
 
+        for (int i = 1; i < list.length; i++) {
+            boolean needForNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j].compareTo(list[j + 1]) > 0) {
+                    E temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    needForNextPass = true;
+                }
+            }
+            if (!needForNextPass) {
+                break;
+            }
+        }
     }
 
     public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator) {
+        for (int i = 1; i < list.length; i++) {
+            boolean needForNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (comparator.compare(list[j], list[j + 1]) > 0) {
+                    E temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    needForNextPass = true;
+                }
+            }
+            if (!needForNextPass) {
+                break;
+            }
+        }
 
     }
 }
